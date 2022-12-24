091a52933e89c40d5eef34a586fdcfbb233857b9
Owais <owaisansari1112@gmail.com>
2022-12-24 15:45:19 +0530

    fix: changed axios baseurl

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Router from "./Router/Router";
 
 function App() {
   // set base url for axios
-  axios.defaults.baseURL = "http://127.0.0.1:5000";
+  axios.defaults.baseURL = "http://127.0.0.1:5000/api/v1";
 
   return (
     <div className="App" style={{ ...styles.app }}>
